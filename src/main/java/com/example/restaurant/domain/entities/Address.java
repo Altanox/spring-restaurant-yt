48b0802ed274b714c6523980674913d6cd1327f2
Lucc <lukasz_rutkowski@live.com>
2026-01-08 15:14:10 +0100

    Create Address entity

---
+++ src/main/java/com/example/restaurant/domain/entities/Address.java
@@ -0,0 +1,33 @@
+package com.example.restaurant.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+    // keyword is used instead of text for when partial text is not involved. Keyword is more efficient
+    @Field(type = FieldType.Keyword)
+    private String streetNumber;
+
+    @Field(type = FieldType.Text)
+    private String streetName;
+
+    @Field(type = FieldType.Keyword)
+    private String unit;
+
+    @Field(type = FieldType.Keyword)
+    private String state;
+
+    @Field(type = FieldType.Keyword)
+    private String postalCode;
+
+    @Field(type = FieldType.Keyword)
+    private String country;
+}
